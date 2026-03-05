26faf3e91f187b0d444c91eb5b6938346e391478
MadhuraneniSaiDinesh <msd010111@gmail.com>
2026-03-05 17:46:14 +0530

    fix api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
@@ -7,9 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", authRoutes);
+app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
